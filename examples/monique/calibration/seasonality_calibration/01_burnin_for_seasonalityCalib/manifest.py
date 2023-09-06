abe08330a4b51f9b9eb68b1f9b72cae6ccc46597
moniqueambrose <mambrose@idmod.org>
2023-09-06 09:56:32 -0700

    ﻿update filepaths and sif_id for Monique's setup

--- examples/monique/calibration/seasonality_calibration/01_burnin_for_seasonalityCalib/manifest.py
+++ examples/monique/calibration/seasonality_calibration/01_burnin_for_seasonalityCalib/manifest.py
@@ -5,17 +5,16 @@ CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "..", "..", "..", "..", ".."))
 
 # change to your input folder which contains the required files...
-download_dir = os.path.join(PROJECT_DIR, "download")
+download_dir = os.path.join(PROJECT_DIR, "downloads")
 schema_file = os.path.join(download_dir, "schema.json")
 eradication_path = os.path.join(download_dir, "Eradication")
 
 # user test data directory
-USER_PATH = None
 # specify user data for testing
 # USER_PATH = r'C:\Projects\emodpy-snt\data'
 
 # load project path
-data_path, project_path = load_box_paths(user_path=USER_PATH, country_name='Example')
+data_path, project_path = load_box_paths(country_name='Example')
 input_dir = os.path.join(project_path, "simulation_inputs")
 
-sif_id = None
+sif_id = '../../../../../dtk_sif.id'
