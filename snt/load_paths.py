900960474668c89d83e4326754ebbfb2128c833c
moniqueambrose <mambrose@idmod.org>
2024-01-04 11:44:39 -0800

    update calibration ranges, filepaths, mismatched LGA names, and code version for compatibility

--- snt/load_paths.py
+++ snt/load_paths.py
@@ -18,10 +18,12 @@ def load_box_paths(user_path=None, country_name='Example'):
         data_path = os.path.join(home_path, 'data')
         project_path = os.path.join(home_path, 'projects', 'burundi_hbhi', 'snt_2023')
     elif country_name == 'Nigeria':
-        home_path = os.path.join(user_path, 'Dropbox (IDM)', 'NU_collaboration')
-        data_path = os.path.join(home_path, 'hbhi_nigeria', 'snt_2022')
-        project_path = os.path.join(home_path, 'hbhi_nigeria', 'snt_2022')
-
+        # home_path = os.path.join(user_path, 'Dropbox (IDM)', 'NU_collaboration')
+        # data_path = os.path.join(home_path, 'hbhi_nigeria', 'snt_2022')
+        # project_path = os.path.join(home_path, 'hbhi_nigeria', 'snt_2022')
+        home_path = os.path.join(user_path, 'Dropbox (IDM)', 'Malaria Team Folder', 'projects', 'snt')
+        data_path = os.path.join(home_path, 'Nigeria', 'snt_2023')
+        project_path = os.path.join(home_path, 'Nigeria', 'snt_2023')
     return data_path, project_path
 
 
